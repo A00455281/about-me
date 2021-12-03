b452b15b21f1586e199a3d32b87448311018c2aa
akshaywaghsmu8995 <akshay.wagh@smu.ca>
2021-12-03 12:10:47 -0400

    added info in about-me and mytown page

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -8,10 +8,13 @@ const AboutMe = () => {
                 <div className="name">
                     <h1>Akshay.</h1>
                 </div>
-                <div>
-                    <h4 className="text-primary name">Software Developer &#128187;</h4>
+                <div className="text-primary name h3">
+                    Software Developer &#128187;
                 </div>
-                <div>I'm a software developer based in Halifax, Canada.</div>
+                <div className="h5">I'm a software developer based in Halifax, NS <br/> currently pursuing my masters at Saint Mary University.</div>
+                <div className="mt-4 h5">
+                    I joined the MCDA because I wanted to explore different fields in <br/>computer science and data analytics.
+                </div>
                 {/* <div>
                     <button type="button" class="btn btn-outline-primary my-4"> Contact Me </button>
                 </div> */}
--- src/components/MyTown.js
+++ src/components/MyTown.js
@@ -35,8 +35,12 @@ class Town extends Component {
         return (
             <div className="d-flex align-items-start flex-column">
                 <img src={halifax} height="200" width="300"/>
-                <div className="mt-4">
+                <div className="mt-4 h3">
                     I live in Halifax, NS Canada
+                </div>
+                <div className="h6 my-3 name">
+                    Halifax is the capital city of Nova Scotia. 
+                    <br/>It is located on the East Coast of Canada on the Atlantic Ocean.
                 </div>
                 <div>
                     {this.state.temperature <= 10 ?
@@ -45,7 +49,17 @@ class Town extends Component {
                                 <img src={mild} alt="Mild" height="40" width="50"/> : 
                                     <img src={sunny} alt="Sunny" height="40" width="50"/>
                     }
-                    <span className="comic-sans primary h3 ml-3">{this.state.temperature}</span>
+                    <span className="comic-sans primary h3 ml-3">
+                        {this.state.temperature}
+                        {/* <div class="btn-group btn-group-toggle" data-toggle="buttons">
+                            <label class="btn btn-secondary active">
+                                <input type="radio" name="options" id="option1" autocomplete="off" checked/> &#8451;
+                            </label>
+                            <label class="btn btn-secondary">
+                                <input type="radio" name="options" id="option2" autocomplete="off"/> &#8457;
+                            </label>
+                        </div> */}
+                    </span>
                 </div>
             </div>
         )
